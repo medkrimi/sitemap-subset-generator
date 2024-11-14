672144e82d683d829f026322752a71ae6c63604b
Mohamed Krimi <contact@medkrimi.net>
2024-11-14 15:52:29 -0500

    Update generated_sitemaps tmp  folder path.

--- src/app/sitemap/[id]/route.ts
+++ src/app/sitemap/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import fs from 'fs/promises'
 import path from 'path'
 
-const SITEMAP_DIR = path.join(process.cwd(), 'generated_sitemaps')
+const SITEMAP_DIR = '/tmp/generated_sitemaps'
 
 export async function GET(
     request: NextRequest,
